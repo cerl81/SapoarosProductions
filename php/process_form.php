a7875d6fa37d1ca384e2b83b497caaac4dd4549b
cesarami81@gmail.com <Gh-15252757>
2023-09-03 21:07:57 -0400

    first commit

---
+++ php/process_form.php
@@ -0,0 +1,4 @@
+<?php function process_form($form_data)
+{if (form_data== true Mysql_query() mysqli_fetch_assoc() mysqli_num_rows())
+{/*Enviar los datos al servidor web/}
+else {/Mostrar un error en la página*/}}?>
---
+++ process_form.php
@@ -0,0 +1,4 @@
+<?php function process_form($form_data)
+{if (form_data== true)
+{/*Enviar los datos al servidor web/}
+else {/Mostrar un error en la página*/}}?>
